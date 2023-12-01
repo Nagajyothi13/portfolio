b5a2ec73f38e463a23b9a85dc0784380e0c2c4be
Nagajyothi <n170264@rguktn.ac.in>
2023-12-01 17:56:38 +0530

    Added Header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
 import './header.css'
-
+import CTA from './CTA'
+import Me from '../../assets/me.jpg'
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className="container header__container">
+        <h5>Hello I'm</h5>
+        <h1>Nagajyothi</h1>
+        <h5 className="text-light">Frontend developer</h5>
+        <CTA/>
+
+        <div className="me">
+          <img src={Me} alt="" />
+        </div>
+        <a href="#contact" className="scroll__down">Scroll Down</a>
+      </div>
+    </header>
   )
 }
 
---
+++ src/components/header/HeaderSocials.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const HeaderSocials = () => {
+  return (
+    <div className="header__socials">
+        <a href="https://www.linkedin.com/in/nagajyothi-b-7b28141b2">L</a>
+        <a href="https://www.github.com/Nagajyothi13" >L</a>
+    </div>
+  )
+}
+
+export default HeaderSocials
