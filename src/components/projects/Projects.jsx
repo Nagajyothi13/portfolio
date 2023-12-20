6d7ec0d2f14ac65d83729d2d566f2c9c32e2d94a
Nagajyothi <n170264@rguktn.ac.in>
2023-12-20 15:28:12 +0530

    added skills icons

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,10 +1,71 @@
-import React from 'react'
-import './projects.css'
+import React from 'react';
+import './projects.css';
 
 const Projects = () => {
   return (
-    <section id='projects'>Projects</section>
-  )
-}
+    <section id='projects'>
+       <div className='heading'>
+        <h1>Projects</h1>
+      </div>
+      <div className="projects-container">
+      <div className='project-card'>
+        <h3 className='project-title'>Artificial Intelligence Integrated Blockchain for Autonomous Vehicles</h3>
+        <p className='project-details'>
+          Skills Used: Blockchain, AI, IoT, Programming
+        </p>
+        <p className='project-details'>
+        Built a decentralized network to empower autonomous vehicles with secure real-time decision-making. This technology combines VIoT, VANET and Blockchain to enhance communication ensuring data integrity, confidentiality, transparency, traceability, and safeguards against unauthorized access and driving autonomous vehicle adoption.
+        </p>
+        <p className='project-source'>
+          Project Source - <a href='https://github.com/Nagajyothi13/AI-Integrated-Blockchain-for-Autonomous-Vehicles' target='_blank' rel='noopener noreferrer'>GitHub</a>
+        </p>
+      </div>
 
-export default Projects
+      <div className='project-card'>
+        <h3 className='project-title'>Topic Modelling on Telugu-English Code-Mixed data using LDA</h3>
+        <p className='project-details'>
+          Skills Used: Python (NumPy, Pandas, Matplotlib, Scikit-learn, genism, nltk, corpus, re, string, etc.), Web Scraping, Math
+        </p>
+        <p className='project-details'>
+        The aim of learning is to discover, from a corpus of documents, good word distributions of the various topics, as well as good topic proportions in the various documents Most of the Topic Modelling techniques applied on English, Hindi, Telugu, Hindi-English code-mixed dataBut there is no proper work on “Telugu-English” code-mixed data. 
+        </p>
+        <p className='project-source'>
+          Project Source - <a href='https://github.com/Nagajyothi13/Topic_modeling_on_telugu_english_code_mixed_data_using_lda' target='_blank' rel='noopener noreferrer'>GitHub</a>
+        </p>
+      </div>
+
+      <div className='project-card'>
+        <h3 className='project-title'>React Portfolio</h3>
+        <p className='project-details'>
+          Skills Used: 
+          HTML, CSS, React
+        </p>
+        <p className='project-details'>
+        Built a decentralized network to empower autonomous vehicles with secure real-time decision-making. This technology combines VIoT, VANET and Blockchain to enhance communication ensuring data integrity, confidentiality, transparency, traceability, and safeguards against unauthorized access and driving autonomous vehicle adoption.
+        </p>
+        
+        <p className='project-source'>
+          Project Source - <a href='https://github.com/Nagajyothi13/Topic_modeling_on_telugu_english_code_mixed_data_using_lda' target='_blank' rel='noopener noreferrer'>GitHub</a>
+        </p>
+      </div>
+
+      <div className='project-card'>
+        <h3 className='project-title'>Topic Modelling on Telugu-English Code-Mixed data using LDA</h3>
+        <p className='project-details'>
+          Skills Used: Python (NumPy, Pandas, Matplotlib, Scikit-learn, genism, nltk, corpus, re, string, etc.), Web Scraping, Math
+        </p>
+        <p className='project-details'>
+        Built a decentralized network to empower autonomous vehicles with secure real-time decision-making. This technology combines VIoT, VANET and Blockchain to enhance communication ensuring data integrity, confidentiality, transparency, traceability, and safeguards against unauthorized access and driving autonomous vehicle adoption.
+        </p>
+        <p className='project-source'>
+          Project Source - <a href='https://github.com/Nagajyothi13/Topic_modeling_on_telugu_english_code_mixed_data_using_lda' target='_blank' rel='noopener noreferrer'>GitHub</a>
+        </p>
+      </div>
+      </div>
+
+      {/* Add more project cards as needed */}
+    </section>
+  );
+};
+
+export default Projects;
--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -1,9 +1,77 @@
-import React from 'react'
+import React from 'react';
+import './skills.css'; // Import the CSS file
+import C from '../../assets/c.png'
+import CPP from '../../assets/cpp.png'
+import java from '../../assets/java.png'
+import python from '../../assets/python.png'
+import js from '../../assets/java-script.png'
+import solidity from '../../assets/solidity-icon.png'
+import html from '../../assets/html-5.png'
+import css from '../../assets/css-3.png'
+import react from '../../assets/react.png'
+import bc from '../../assets/blockchain.png'
+import nlp from '../../assets/nlp.png'
+import mysql from '../../assets/mysql.png'
+import msofc from '../../assets/office.png'
+import github from '../../assets/github.png'
+import vs from '../../assets/visual-studio-code.png'
+
 
 const Skills = () => {
   return (
-    <section id='skills'>Skills</section>
-  )
-}
+    <section id='skills'>
+      <div>
+        <h2>Skills</h2>
+      </div>
+      <div className='skills-container'>
+        <div className='skill-card'>
+          <h3>Programming Languages</h3>
+          <ul className='skill-list'>
+            <li className='skill-item'><img src={C} alt=" "/>C</li>
+            <li className='skill-item'><img src={CPP} alt=" "/>C++</li>
+            <li className='skill-item'><img src={java} alt=" "/>Java</li>
+            <li className='skill-item'><img src={python} alt=" "/>Python</li>
+            <li className='skill-item'><img src={js} alt=" "/>JavaScript</li>
+            <li className='skill-item'><img src={solidity} alt=" "/>Solidity</li>
+          </ul>
+        </div>
 
-export default Skills
+        <div className='skill-card'>
+          <h3>Web Technologies</h3>
+          <ul className='skill-list'>
+            <li className='skill-item'><img src={html} alt=" "/>HTML</li>
+            <li className='skill-item'><img src={css} alt=" "/>CSS</li>
+            <li className='skill-item'><img src={js} alt=" "/>JavaScript</li>
+            <li className='skill-item'><img src={react} alt=" "/>React</li>
+          </ul>
+        </div>
+
+        <div className='skill-card'>
+          <h3>Other Technologies</h3>
+          <ul className='skill-list'>
+            <li className='skill-item'><img src={bc} alt=" "/>Blockchain</li>
+            <li className='skill-item'><img src={nlp} alt=" "/>NLP</li>
+          </ul>
+        </div>
+
+        <div className='skill-card'>
+          <h3>Databases</h3>
+          <ul className='skill-list'>
+            <li className='skill-item'><img src={mysql} alt=" "/>MySQL</li>
+          </ul>
+        </div>
+
+        <div className='skill-card'>
+          <h3>Others</h3>
+          <ul className='skill-list'>
+            <li className='skill-item'><img src={msofc} alt=" "/>Ms Office</li>
+            <li className='skill-item'><img src={github} alt=" "/>Github</li>
+            <li className='skill-item'><img src={vs} alt=" "/>VS Code</li>
+          </ul>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Skills;
